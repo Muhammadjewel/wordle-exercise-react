95f4d2d3746403498cddd2c97aa5062fa0780a32
muhammadjewel <msuratov@gmail.com>
2023-11-30 18:14:01 +0500

    Display attempt cells

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function GuessResults({ results }) {
+  const remainingAttempts = NUM_OF_GUESSES_ALLOWED - results.length;
+  console.log({ remainingAttempts });
+
   return (
     <div className="guess-results">
       {results.map(({ label, id }) => (
         <p key={id} className="guess">
-          {label}
+          {label.split("").map((char) => (
+            <span className="cell">{char}</span>
+          ))}
         </p>
       ))}
+
+      {remainingAttempts > 0 &&
+        range(remainingAttempts).map((attempt, index) => (
+          <p key={index} className="guess">
+            {range(5).map((char) => (
+              <span className="cell"></span>
+            ))}
+          </p>
+        ))}
     </div>
   );
 }
